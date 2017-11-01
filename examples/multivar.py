9558e6379e8cd1596cd34136a5578b1c3690aa43
Flávio Codeço Coelho <fccoelho@gmail.com>
2017-11-01 15:06:31 -0200

    minor changes

--- examples/multivar.py
+++ examples/multivar.py
@@ -3,11 +3,11 @@
 from pylab import *
 from ddeint import ddeint
 
-model = lambda Y,t:  -Y( t-3*cos( Y(t) )**2 )
+model = lambda Y, t: -Y(t - 3 * cos(Y(t)) ** 2)
 tt = linspace(0, 30, 2000)
-yy = ddeint(model, lambda t:1, tt)
+yy = ddeint(model, lambda t: 1, tt)
 
-fig, ax = subplots(1,figsize=(4,4))
+fig, ax = subplots(1, figsize=(4, 4))
 ax.plot(tt, yy)
-    
+
 show()
